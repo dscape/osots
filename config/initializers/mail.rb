a2a53ab67466102762eaa0cbed0e477b92453771
Nuno Job <null@dscape.(none)>
2008-05-05 12:33:25 +0100

    Started on SMTP integration so no mail.rb for you sir

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,10 +1,10 @@
 # Email settings
-# ActionMailer::Base.delivery_method = :smtp
-# ActionMailer::Base.smtp_settings = {
-#   :address => "mail.yourapplication.com",
-#   :port => 25,
-#   :domain => "yourapplication.com",
-#   :authentication => :login,
-#   :user_name => "mail@yourapplication.com",
-#   :password => "yourapplicationpassword"  
+ ActionMailer::Base.delivery_method = :smtp
+ ActionMailer::Base.smtp_settings = {
+   :address => "smtp.gmail.com",
+   :port => 25,
+   :domain => "yourapplication.com",
+   :authentication => :login,
+   :user_name => "db2minho",
+   :password => "sho=nupe"  
 # }
